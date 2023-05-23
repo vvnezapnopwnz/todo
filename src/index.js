028a0576901bdce55804cfb4092ad4d5cfa4c577
vvnezapnopwnz <vvnezapnopwnz@gmail.com>
2023-05-23 17:38:50 +0600

    ispravil nekotorie faili v papke s proektom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const TodoList = () => {
+  return (
+    <ul>
+      <li>My first task</li>
+      <li>do homework</li>
+    </ul>
+  );
+};
+const AppHeader = () => {
+  return <h1>Todo App</h1>;
+};
+const SearchPanel = () => {
+  return <input placeholder="search task" />;
+};
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <AppHeader/>
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
